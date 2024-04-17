a16b38205f6ab3f885f0bd1a4bd6b0e3453bf37d
exmdr <163081484+exmdr@users.noreply.github.com>
2024-04-17 18:38:52 +0800

    Update and rename init.ps1 to init.bat

--- init.ps1
+++
@@ -1,9 +0,0 @@
-for ($i = 0; $i -lt 24; $i++)
-{
-    $JsonTime = (Get-date).ToString('yyyy-MM-dd')
-    if ($i -lt 10) {
-	mkdir "C:\home\site\wwwroot\F5XC\date\$JsonTime\hour\0$i" 
-    } else {
-	mkdir "C:\home\site\wwwroot\F5XC\date\$JsonTime\hour\$i" 
-    }
-}
